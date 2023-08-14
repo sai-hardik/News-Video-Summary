e852c25e2c6308312f0424dc7b39c536962754de
RushilRanjan14 <85254441+RushilRanjan14@users.noreply.github.com>
2023-08-15 00:26:54 +0530

    creating transcripts using assembly ai

---
+++ api_communication.py
@@ -0,0 +1,66 @@
+import requests
+import time
+from api_secrets import API_KEY_ASSEMBLYAI
+
+
+upload_endpoint = 'https://api.assemblyai.com/v2/upload'
+transcript_endpoint = 'https://api.assemblyai.com/v2/transcript'
+
+
+headers = {"authorization": API_KEY_ASSEMBLYAI,}
+
+
+def upload(filename):
+    def read_file(filename,CHUNK_SIZE = 5_242_880):
+        with open(filename, 'rb') as _file:
+            while True:
+                data = _file.read(CHUNK_SIZE)
+                if not data:
+                    break
+                yield data
+
+    upload_response = requests.post(upload_endpoint,
+                                    headers=headers, 
+                                    data=read_file(filename))
+    audio_url = upload_response.json()['upload_url']
+    return audio_url
+
+
+def transcribe(audio_url):
+    transcript_request = {'audio_url': audio_url}
+
+    transcript_response = requests.post(transcript_endpoint, json=transcript_request, headers=headers)
+    job_id = transcript_response.json()['id']
+    return job_id
+
+        
+def poll(transcript_id):
+    polling_endpoint = transcript_endpoint + '/' + transcript_id
+    polling_response = requests.get(polling_endpoint, headers=headers)
+    return polling_response.json()
+
+        
+
+def get_transcription_result_url(audio_url):
+    transcribe_id = transcribe(audio_url)
+    while True:
+        data = poll(transcribe_id)
+        if data['status'] == 'completed':
+            return data, None
+        elif data['status'] == 'error':
+            return data, data['error']
+            
+        print("waiting for 30 seconds")
+        time.sleep(30)
+        
+        
+def save_transcript(audio_url, filename):
+    data, error = get_transcription_result_url(audio_url)
+    
+    if data:
+        text_filename = filename + '.txt'
+        with open(text_filename, 'w') as f:
+            f.write(data['text'])
+        print('Transcript saved')
+    elif error:
+        print("Error!!!", error)
